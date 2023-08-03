39ab52c9458bee1537d085e48a3750b20bc83481
Pusikovsa <s.puzikov59@gmail.com>
2023-08-03 14:40:40 +0300

    added hive adapter

---
+++ lib/models/place.g.dart
@@ -0,0 +1,90 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'place.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PlaceAdapter extends TypeAdapter<Place> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Place read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Place(
+      id: fields[0] as String,
+      title: fields[1] as String,
+      location: fields[2] as PlaceLocation?,
+      image: fields[3] as File,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Place obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.location)
+      ..writeByte(3)
+      ..write(obj.image);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PlaceAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class PlaceLocationAdapter extends TypeAdapter<PlaceLocation> {
+  @override
+  final int typeId = 1;
+
+  @override
+  PlaceLocation read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PlaceLocation(
+      latitude: fields[0] as double,
+      longitude: fields[1] as double,
+      address: fields[2] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, PlaceLocation obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.latitude)
+      ..writeByte(1)
+      ..write(obj.longitude)
+      ..writeByte(2)
+      ..write(obj.address);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PlaceLocationAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
