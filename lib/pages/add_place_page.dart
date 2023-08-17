33b3af96a3f510f1c231367cb425bc1766771d82
Pusikovsa <s.puzikov59@gmail.com>
2023-08-17 15:15:44 +0300

    added LocationInput on add_place_page.dart

--- lib/pages/add_place_page.dart
+++ lib/pages/add_place_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:my_adventures/providers/my_places.dart';
 import 'package:provider/provider.dart';
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 class AddPlacePage extends StatefulWidget {
   static const String rout = '/add_place';
@@ -54,10 +55,14 @@ class _AddPlacePageState extends State<AddPlacePage> {
                       ),
                       controller: _titleController,
                     ),
-                    SizedBox(
-                      height: 200,
-                      child: ImageInput(onSelectImage: _selectImage),
+                    const SizedBox(
+                      height: 10,
                     ),
+                    ImageInput(onSelectImage: _selectImage),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const LocationInput(),
                   ],
                 ),
               ),
